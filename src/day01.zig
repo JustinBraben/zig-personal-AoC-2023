04009247279e779c98fe1da84a885e01f01b7dec
Justin Braben <jbraben@dmsiworks.com>
2024-01-03 21:06:48 -0700

    Day1 full and Day2 part 1

--- src/day01.zig
+++ src/day01.zig
@@ -11,7 +11,77 @@ const gpa = util.gpa;
 const data = @embedFile("data/day01.txt");
 
 pub fn main() !void {
-    
+    print("\n", .{});
+
+    var lines = splitSeq(u8, data, "\n");
+
+    var answer1: u32 = 0;
+    var answer2: u32 = 0;
+
+    while (lines.next()) |line| {
+        print("{s}\n", .{line});
+        answer1 += parseLinePart1(line);
+        answer2 += parseLinePart2(line);
+    }
+
+    print("Answer to part 1 : {}\n", .{answer1});
+    print("Answer to part 2 : {}\n", .{answer2});
+}
+
+fn parseLinePart1(line: []const u8) u32 {
+    var res: u32 = 0;
+    var prevDigit: u8 = '0';
+    var firstFound: bool = false;
+
+    for (line) |c| {
+        if (std.ascii.isDigit(c)) {
+            prevDigit = c;
+
+            if (!firstFound) {
+                res += 10 * (c - '0');
+                firstFound = true;
+            }
+
+            //print("{c}, ", .{c});
+        }
+    }
+
+    res += prevDigit - '0';
+
+    print("\n", .{});
+
+    return res;
+}
+
+fn parseLinePart2(line: []const u8) u32 {
+    var res: u32 = 0;
+    var first: ?u8 = null;
+    var last: ?u8 = null;
+
+    var indexLine: usize = 0;
+
+    const digit_names = [_][]const u8{ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+    while (indexLine < line.len) : (indexLine += 1) {
+        const curChar: u8 = line[indexLine];
+        if (std.ascii.isDigit(curChar)) {
+            last = curChar - '0';
+            first = first orelse last;
+            continue;
+        }
+
+        for (digit_names, 0..) |name, indexDigitNames| {
+            if (std.mem.startsWith(u8, line[indexLine..], name)) {
+                last = @as(u8, @intCast(indexDigitNames));
+                first = first orelse last;
+                break;
+            }
+        }
+    }
+
+    res = 10 * (first orelse 0) + (last orelse 0);
+
+    return res;
 }
 
 // Useful stdlib functions
--- src/day02.zig
+++ src/day02.zig
@@ -10,8 +10,197 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day02.txt");
 
+const ParseErr = error{
+    TooManyParts,
+    InvalidItem,
+};
+
+const Round = struct {
+    num_red: u32,
+    num_green: u32,
+    num_blue: u32,
+};
+
+const Game = struct {
+    id: u32,
+    rounds: ?[]Round,
+};
+
+const digit_names = [_][]const u8{ "red", "green", "blue" };
+
 pub fn main() !void {
-    
+    print("\n", .{});
+
+    var res: u32 = 0;
+
+    const ideal_set = Round{
+        .num_red = 12,
+        .num_green = 13,
+        .num_blue = 14,
+    };
+
+    var lines = splitSeq(u8, data, "\n");
+
+    //print("Possible Game | red, {} | green, {} | blue, {}\n", .{ ideal_set.num_red, ideal_set.num_green, ideal_set.num_blue });
+
+    while (lines.next()) |line| {
+        if (line.len == 0) {
+            continue;
+        }
+
+        //const g = try makeGame(line);
+
+        //print("Current Game {} | red, {} | green, {} | blue, {} | Possible : {}\n", .{ g.id, g.num_red, g.num_green, g.num_blue, try parseGameBool(line, ideal_set) });
+        //print("Game is possible : {}\n", .{try parseGameBool(line, ideal_set)});
+
+        //res += try parseGame(line, ideal_set);
+        res += try parseGameRounds(line, ideal_set);
+        //res += 1;
+
+        //const game = try makeGame(line);
+        //print("{}\n", .{game});
+    }
+
+    print("Answer to Part 1 : {}\n", .{res});
+}
+
+fn parseGameRounds(line: []const u8, bag: Round) !u32 {
+    var g = Game{
+        .id = 0,
+        .rounds = null,
+    };
+
+    var line_tokens = tokenizeSeq(u8, line, ": ");
+
+    const game_str = line_tokens.next() orelse return ParseErr.InvalidItem;
+    const sets_str = line_tokens.next() orelse return ParseErr.InvalidItem;
+    if (line_tokens.next() != null) return ParseErr.TooManyParts;
+
+    // game_str[5..] gets a slice of the game id, from index 5 of the array to the end
+    g.id = try parseInt(u32, game_str[5..], 10);
+
+    var round_tokens = tokenizeSeq(u8, sets_str, "; ");
+
+    while (round_tokens.next()) |round_token| {
+        var in_round_tokens = tokenizeSeq(u8, round_token, ", ");
+
+        //print("Rounds : | {s}\n", .{round_token});
+
+        while (in_round_tokens.next()) |in_round_token| {
+            var num_cube_tokens = tokenizeSeq(u8, in_round_token, " ");
+            const cube_quantity_str = num_cube_tokens.next() orelse return ParseErr.InvalidItem;
+            const cube_color_str = num_cube_tokens.next() orelse return ParseErr.InvalidItem;
+
+            if (std.mem.eql(u8, cube_color_str, "red")) {
+                const round_quantity = try parseInt(u8, cube_quantity_str[0..], 10);
+                if (round_quantity > bag.num_red) {
+                    //print("Failed Game {}, needs {} red cubes but only have {}\n", .{ g.id, round_quantity, bag.num_red });
+                    return 0;
+                }
+            }
+            if (std.mem.eql(u8, cube_color_str, "green")) {
+                const round_quantity = try parseInt(u8, cube_quantity_str[0..], 10);
+                if (round_quantity > bag.num_green) {
+                    //print("Failed Game {}, needs {} green cubes but only have {}\n", .{ g.id, round_quantity, bag.num_green });
+                    return 0;
+                }
+            }
+            if (std.mem.eql(u8, cube_color_str, "blue")) {
+                const round_quantity = try parseInt(u8, cube_quantity_str[0..], 10);
+                if (round_quantity > bag.num_blue) {
+                    //print("Failed Game {}, needs {} blue cubes but only have {}\n", .{ g.id, round_quantity, bag.num_blue });
+                    return 0;
+                }
+            }
+        }
+    }
+
+    //print("Succesful Game : | {}\n", .{g.id});
+    return g.id;
+}
+
+fn parseGameBool(line: []const u8, bag: Round) !bool {
+    const g = try makeGame(line);
+
+    if (g.num_red < bag.num_red) {
+        return false;
+    }
+
+    if (g.num_blue < bag.num_blue) {
+        return false;
+    }
+
+    if (g.num_green < bag.num_green) {
+        return false;
+    }
+
+    return true;
+}
+
+fn parseGame(line: []const u8, bag: Round) !u32 {
+    const g = try makeGame(line);
+
+    if (g.num_red < bag.num_red) {
+        return 0;
+    }
+
+    if (g.num_blue < bag.num_blue) {
+        return 0;
+    }
+
+    if (g.num_green < bag.num_green) {
+        return 0;
+    }
+
+    return g.id;
+}
+
+fn makeGame(line: []const u8) !Game {
+    var g = Game{
+        .id = 0,
+        .num_red = 0,
+        .num_green = 0,
+        .num_blue = 0,
+    };
+
+    var line_tokens = tokenizeSeq(u8, line, ": ");
+
+    const game_str = line_tokens.next() orelse return ParseErr.InvalidItem;
+    const sets_str = line_tokens.next() orelse return ParseErr.InvalidItem;
+    if (line_tokens.next() != null) return ParseErr.TooManyParts;
+
+    // game_str[5..] gets a slice of the game id, from index 5 of the array to the end
+    g.id = try parseInt(u32, game_str[5..], 10);
+
+    //print("{s}, {s}\n", .{ game_str, sets_str });
+
+    var sets_tokens = tokenizeSeq(u8, sets_str, "; ");
+
+    while (sets_tokens.next()) |set_str| {
+
+        //print("Game : {} | Sets : {s}\n", .{ g.id, set_str });
+        var in_set_tokens = tokenizeSeq(u8, set_str, ", ");
+
+        while (in_set_tokens.next()) |in_set_str| {
+            //print("Game : {} | Sets : {s}\n", .{ g.id, in_set_str });
+            var num_cube_tokens = tokenizeSeq(u8, in_set_str, " ");
+            const cube_quantity_str = num_cube_tokens.next() orelse return ParseErr.InvalidItem;
+            const cube_color_str = num_cube_tokens.next() orelse return ParseErr.InvalidItem;
+            if (num_cube_tokens.next() != null) return ParseErr.TooManyParts;
+
+            if (std.mem.eql(u8, cube_color_str, "red")) {
+                g.num_red += try parseInt(u8, cube_quantity_str[0..], 10);
+            }
+            if (std.mem.eql(u8, cube_color_str, "green")) {
+                g.num_green += try parseInt(u8, cube_quantity_str[0..], 10);
+            }
+            if (std.mem.eql(u8, cube_color_str, "blue")) {
+                g.num_blue += try parseInt(u8, cube_quantity_str[0..], 10);
+            }
+        }
+    }
+
+    return g;
 }
 
 // Useful stdlib functions
